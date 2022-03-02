3f7446a78f60e5608acd5d11a5cd62b1ac049987
balamanikandan9232 <balamanikandan9232@gmail.com>
2022-03-02 13:19:00 +0530

    listout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import './App.css'
+import { Link, Route, Switch } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        <li>
+          <Link to="/recipe-list"> Recipe List </Link>
+        </li>
+        <li>
+          <Link to="/"> Home </Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route path="/recipe-list">
+          <RecipeList />
+        </Route>
+        <Route path="/">
+          <Welcome />
+        </Route>
+      </Switch>
+      {/* <Welcome />
+      <RecipeList /> */}
     </div>
   );
 }
 
-export default App;
+function Welcome() {
+  const message = "Welcome to Recipe List 🍗"
+  return (
+    <div>
+      <h1>{message}</h1>
+    </div>
+  );
+}
+function RecipeList() {
+  const message = "Awesome Recipe List";
+  const [recipeList, setRecipeList] = useState([])
+  useEffect(() => {
+    fetch("https://61d28820da87830017e595e3.mockapi.io/recipes")
+      .then((data) => data.json())
+      .then((recipes) => setRecipeList(recipes));
+  }, [])
+  return (
+    <div>
+      <h1>{message}</h1>
+      <div className='recipe-list-container'>
+        {recipeList.map(recipe => <Recipe recipe={recipe} />)}
+      </div>
+    </div>
+  );
+}
+function Recipe({ recipe }) {
+  return (
+    <div className='recipe-container'>
+      <img src={recipe.picture} alt={recipe.name} className='Recipe-picture' />
+      <p className='Recipe-Name'>{recipe.name}</p>
+    </div>
+  )
+}
